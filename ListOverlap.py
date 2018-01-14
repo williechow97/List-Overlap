3721d468196699a68a2f236eaf2d5eff2cb6c139
williechow97 <willie.chow01@gmail.com>
2018-01-14 01:11:21 -0800

    Update ListOverlap.py
    Added comments.
    Changed to use more method of random module
    Updated generateRanList() to make list of numbers in random order and size

--- ListOverlap.py
+++ ListOverlap.py
@@ -16,8 +16,8 @@ def generateRanList():
     '''create two randomly generated list with values ranging from 0 - 99
        returns tuple of two list
     '''
-    l1 = [elm for elm in range(random.randint(1,100))]
-    l2 = [elm for elm in range(random.randint(1,100))]
+    l1 = random.sample(range(100), random.randint(1,100))    # create a list of random numbers (0 - 99) of a random size (1 - 100). 
+    l2 = random.sample(range(100), random.randint(1,100))    # .sample(range, size) creates a list
     print('l1', l1)
     print('l2', l2)
     return (l1,l2)
@@ -26,10 +26,16 @@ def listOverlap(a,b):
     '''@param: a tuple of two list
         prints out a list of the overlap of the two list in the parameter tuple
     '''
-    overlap = [elm for elm in a if elm in b]
+    overlap = [elm for elm in a if elm in b]    #list comprehension to place overlapping elem in two list into a new one
+    overlap.sort()  # sort the list of overlap 
     print('overlap',overlap)
     
 def main():
-    listOverlap(*generateRanList()) 
+    a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]    
+    print('List 1:', a, '\nList 2:', b)
+    listOverlap(a,b)
+    print('\nRandomly Generated List: ')
+    listOverlap(*generateRanList()) # * - unpacks the tuple into two strings...to be used a parameters for function call
 
 main()
